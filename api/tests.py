2c4e001aa49c505c414f095083bcb640d8a6aa91
Dilshad <dil_shad@Asus.domain.name>
2022-07-08 09:48:23 +0530

    test done

--- api/tests.py
+++ api/tests.py
@@ -1,39 +1,90 @@
-from urllib import response
+
 from django.test import TestCase
 import requests
+from requests.auth import HTTPBasicAuth
 # Create your tests here.
 
 response = requests.get("http://127.0.0.1:8000/api/book-list")
 # print(type(response))
-#print(response.json())
-
+# print(response.json())
 
 res = requests.get("http://127.0.0.1:8000/api/book-details/1")
 # print(res.url)
 
 
+# filter by genre
 p = {'genre': 'Thriller'}
 
-filter_resp = requests.get("http://127.0.0.1:8000/api/get_books", params=p)
-# print(filter_resp.json())
+
+def filter_val(arg):
+    filter_resp = requests.get("http://127.0.0.1:8000/api/get_books", params=p)
+    return filter_resp.json()
+
+# print(filter_val(p))
 
 
-payload = {"username": "user_req", "email": "req611606@gmail.com",
-           "password": "user123", "password2": "user123"}
+# create user
+payload = {"username": "user_new", "email": "rq611606@gmail.com",
+           "password": "user_new", "password2": "user_new"}
 
 
-#Create User
-post_resp = requests.post("http://127.0.0.1:8000/api/RegisterUser", data=payload)
-#print(post_resp.json() )
+def create_user(arg):
+    post_resp = requests.post("http://127.0.0.1:8000/api/RegisterUser", data=payload)
+    return post_resp.json()
+
+# print(create_user(payload))
 
 
+# update_books
+update_payload = {
+    "title": "API testing",
+    "author": "API TTT",
+    "review": 5,
+    "favorite": True,
+    "genre": 3
+}
 
-update_payload={
-        "title": "API testing",
-        "author": "API",
-        "review": 5,
-        "favorite": True,
-        "genre": 3
-    }
-update_resp=requests.put("http://127.0.0.1:8000/api/book-update/6",data=update_payload)
-print(update_resp.json())
+
+def update_book(id, payload):
+    update_resp = requests.put(f"http://127.0.0.1:8000/api/book-update/{id}", data=payload)
+    print(update_resp.json())
+
+# update_book(17,update_payload)
+
+
+# delete
+
+def delete_book(arg):
+    del_resp = requests.delete(f"http://127.0.0.1:8000/api/book-delete/{arg}")
+    print(del_resp.status_code)
+    return del_resp.json()
+
+# print(delete_book(18))
+
+
+# auth_user
+log = {
+    'username': 'test_user',
+    'password': 'test123'
+}
+
+
+def user_auth(log):
+    log_res = requests.post('http://127.0.0.1:8000/api/login/', data=log)
+    #print(log_res.status_code)
+    r = log_res.json()
+    return r
+
+
+tok = user_auth(log)
+print(tok)
+
+
+# authorization
+def authorization_request(arg):
+    res = requests.get('http://127.0.0.1:8000/api/welcome',headers={'Authorization': f'token {arg}'})
+
+    return res.json()
+
+
+print(authorization_request(tok['token']))
